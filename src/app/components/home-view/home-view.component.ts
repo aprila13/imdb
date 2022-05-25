c187ca07ce8c76686cfc2c6a8d3acb9ced1a31e3
April Craig <aprilacraig1@gmail.com>
2022-05-25 14:23:23 -0400

    Update home-view.component.ts

--- src/app/components/home-view/home-view.component.ts
+++ src/app/components/home-view/home-view.component.ts
@@ -8,23 +8,23 @@ import { UsersService } from '../../services/users.service';
 })
 export class HomeViewComponent implements OnInit {
 //Prod
-  data: any = [];
-  constructor(private user: UsersService) {
-    this.user.getData().subscribe((data) => {
-      this.data = data['items']; //test
-      console.log(data);
-    });
-  }
-
-  //test
   // data: any = [];
   // constructor(private user: UsersService) {
   //   this.user.getData().subscribe((data) => {
-  //     //this.propAbove = apiData;
-  //     this.data = data; 
+  //     this.data = data['items']; //test
   //     console.log(data);
   //   });
   // }
+
+  //test
+  data: any = [];
+  constructor(private user: UsersService) {
+    this.user.getData().subscribe((data) => {
+      //this.propAbove = apiData;
+      this.data = data; 
+      console.log(data);
+    });
+  }
 
   ngOnInit(): void {
   }
